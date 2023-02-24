84c21a44a91314b595c5160a1d9eba2c94858166
Spacepineapple <neal.t.reeves@gmail.com>
2023-02-24 20:36:46 +0000

    Add init function and logic to gather manager details and prompt for additional team members

--- index.mjs
+++ index.mjs
@@ -1,15 +1,15 @@
-import Manager from "./lib/Manager";
-import Engineer from "./lib/Engineer";
-import Intern from "./lib/Intern";
-import fs from "fs";
+//import Manager from "./lib/Manager";
+//import Engineer from "./lib/Engineer";
+//import Intern from "./lib/Intern";
+//import fs from "fs";
 import inquirer from "inquirer";
 import path from "path";
 
 
-const OUTPUT_DIR = path.resolve(__dirname, "output");
-const outputPath = path.join(OUTPUT_DIR, "team.html");
+//const OUTPUT_DIR = path.resolve(__dirname, "output");
+//const outputPath = path.join(OUTPUT_DIR, "team.html");
 
-const render = require("./src/page-template.js");
+//const render = require("./src/page-template.js");
 
 const questions = [
     "What is the employee's name?",
@@ -51,9 +51,31 @@ async function init() {
             name: "office",
             message: office,
         },
-    ])
-    team.append(manager_details);
-    //while (team_building) {
-
-    //}
+    ]).then(val => {
+        team.push(val);
+    }).then(() =>
+        addToTeam()
+    )
+    .then(val => {            
+        console.log(val)
+    }
+    );
 }
+
+async function addToTeam() {
+    return await inquirer
+    .prompt([
+        {
+            type: "list",
+            name: "teamBuild",
+            message: "Add to team?",
+            choices: [
+                "Add engineer",
+                "Add intern",
+                "Finish building team",
+            ],
+        },
+    ])
+}
+
+init();
