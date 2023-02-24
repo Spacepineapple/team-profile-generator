053c32ffbc8411833abe2b7439b34306ccce9343
Spacepineapple <neal.t.reeves@gmail.com>
2023-02-24 19:03:22 +0000

    Add questions, question variables and basic inquirer functionality to index.mjs

--- index.mjs
+++ index.mjs
@@ -11,6 +11,49 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
 
+const questions = [
+    "What is the employee's name?",
+    "What is the employee's employee ID?",
+    "What is the employee's email address?",
+    "What is the employee's office number?",
+    "What is the employee's GitHub username?",
+    "What is the employee's school?",
+];
+
+let [name, employeeID, email, office, github, school] = questions;
+
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
+async function init() {
+    //Asynchronously assign the inquirer prompt responses to a variable
+    let team = [];
+    let team_building = true;
+    let manager_details = await inquirer
+    .prompt([
+        {
+            type: "input",
+            name: "name",
+            message: name,
+        },
+        {
+            type: "input",
+            name: "employeeID",
+            message: employeeID,
+        },
+        {
+            type: "input",
+            name: "email",
+            message: email,
+        },
+        {
+            type: "input",
+            name: "office",
+            message: office,
+        },
+    ])
+    team.append(manager_details);
+    //while (team_building) {
+
+    //}
+}
